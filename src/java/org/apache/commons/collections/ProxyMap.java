312f353b21960fd9f7fa8bf6bb0e4ae9670b3356
Morgan James Delagrange <morgand@apache.org>
2002-03-15 05:36:58 +0000

    added for collections 2.0
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@130646 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/collections/ProxyMap.java
@@ -0,0 +1,161 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/ProxyMap.java,v 1.1 2002/03/15 05:36:58 morgand Exp $
+ * $Revision: 1.1 $
+ * $Date: 2002/03/15 05:36:58 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.commons.collections;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/** 
+ * <p>delegate - a person authorized to act as representative for
+ * another</p>
+ *
+ * <p>This <code>Map</code> wraps another <code>Map</code>
+ * implementation, using the wrapped instance for its default
+ * implementation.  This class is used as a framework on which to
+ * build to extensions for its wrapped <code>Map</code> object which
+ * would be unavailable or inconvenient via sub-classing (but usable
+ * via composition).</p>
+ *
+ * <p>An example use case is where the wrapped <code>Map</code> needs
+ * synchronization (to make it thread-safe), but the <code>Map</code>
+ * returned by <code>Collections.synchronizedMap(map)</code>
+ * hides part of <code>map</code>'s public interface.</p>
+ *
+ * @author <a href="mailto:dlr@collab.net">Daniel Rall</a>
+ * @since 2.0
+ */
+public abstract class ProxyMap implements Map {
+    
+    /**
+     * The <code>Map</code> used for default implementations.
+     */
+    protected Map map;
+
+    /**
+     * Creates a new instance acting as a representative for the
+     * specified <code>Map</code>.
+     *
+     * @param map The <code>Map</code> to whose operations to wrap.
+     */
+    public ProxyMap(Map map) {
+        this.map = map;
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public boolean containsKey(Object key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        return map.containsValue(value);
+    }
+
+    public Set entrySet() {
+        return map.entrySet();
+    }
+
+    public boolean equals(Object map) {
+        return map.equals(map);
+    }
+
+    public Object get(Object key) {
+        return map.get(key);
+    }
+
+    public int hashCode() {
+        return map.hashCode();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public Set keySet() {
+        return map.keySet();
+    }
+
+    public Object put(Object key, Object value) {
+        return map.put(key, value);
+    }
+
+    public void putAll(Map t) {
+        map.putAll(t);
+    }
+
+    public Object remove(Object key) {
+        return map.remove(key);
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public Collection values() {
+        return map.values();
+    }
+   
+}
+
+
