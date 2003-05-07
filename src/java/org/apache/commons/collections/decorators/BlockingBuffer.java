51ace983265d796fa76b5b113a9ee797fd250b4d
Stephen Colebourne <scolebourne@apache.org>
2003-05-07 12:19:26 +0000

    Add Buffer collection decorators
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131047 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/collections/decorators/BlockingBuffer.java
@@ -0,0 +1,140 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/decorators/Attic/BlockingBuffer.java,v 1.1 2003/05/07 12:19:26 scolebourne Exp $
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.commons.collections.decorators;
+
+import java.util.Collection;
+
+import org.apache.commons.collections.Buffer;
+import org.apache.commons.collections.BufferUnderflowException;
+
+/**
+ * <code>BlockingBuffer</code> decorates another <code>Buffer</code>
+ * to block on calls to the get method to wait until entries are
+ * added to the buffer.
+ *
+ * @since Commons Collections 3.0
+ * @version $Revision: 1.1 $ $Date: 2003/05/07 12:19:26 $
+ * 
+ * @author Stephen Colebourne
+ */
+public class BlockingBuffer extends SynchronizedBuffer {
+    
+    /**
+     * Factory method to create a blocking buffer.
+     * 
+     * @param buffer  the buffer to decorate, must not be null
+     * @throws IllegalArgumentException if buffer is null
+     */
+    public static Buffer decorate(Buffer buffer) {
+        return new BlockingBuffer(buffer);
+    }
+    
+    /**
+     * Constructor that wraps (not copies).
+     * 
+     * @param buffer  the buffer to decorate, must not be null
+     * @throws IllegalArgumentException if the buffer is null
+     */
+    protected BlockingBuffer(Buffer buffer) {
+        super(buffer);
+    }
+
+    //-----------------------------------------------------------------------
+    public boolean add(Object o) {
+        synchronized (lock) {
+            boolean result = collection.add(o);
+            notify();
+            return result;
+        }
+    }
+    
+    public boolean addAll(Collection c) {
+        synchronized (lock) {
+            boolean result = collection.addAll(c);
+            notifyAll();
+            return result;
+        }
+    }
+    
+    public Object get() {
+        synchronized (lock) {
+            while (collection.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new BufferUnderflowException();
+                }
+            }
+            return getBuffer().get();
+        }
+    }
+    
+    public Object remove() {
+        synchronized (lock) {
+            while (collection.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new BufferUnderflowException();
+                }
+            }
+            return getBuffer().remove();
+        }
+    }
+    
+}
