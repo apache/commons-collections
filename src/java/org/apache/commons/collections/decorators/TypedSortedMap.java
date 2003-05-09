ebc5684efde6b30b45b15d0b37c0f48071e5a0c6
Stephen Colebourne <scolebourne@apache.org>
2003-05-09 16:42:36 +0000

    Add Map and SortedMap collection decorators
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131054 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/collections/decorators/TypedSortedMap.java
@@ -0,0 +1,104 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/decorators/Attic/TypedSortedMap.java,v 1.1 2003/05/09 16:42:35 scolebourne Exp $
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.commons.collections.decorators;
+
+import java.util.SortedMap;
+
+/**
+ * <code>TypedSortedMap</code> decorates another <code>SortedMap</code>
+ * to validate that elements added are of a specific type.
+ * <p>
+ * The validation of additions is performed via an instanceof test against 
+ * a specified <code>Class</code>. If an object cannot be addded to the
+ * collection, an IllegalArgumentException is thrown.
+ *
+ * @since Commons Collections 3.0
+ * @version $Revision: 1.1 $ $Date: 2003/05/09 16:42:35 $
+ * 
+ * @author Stephen Colebourne
+ * @author Matthew Hawthorne
+ */
+public class TypedSortedMap {
+
+    /**
+     * Factory method to create a typed sorted map.
+     * <p>
+     * If there are any elements already in the map being decorated, they
+     * are validated.
+     * 
+     * @param map  the map to decorate, must not be null
+     * @param keyType  the type to allow as keys, must not be null
+     * @param valueType  the type to allow as values, must not be null
+     * @throws IllegalArgumentException if list or type is null
+     * @throws IllegalArgumentException if the list contains invalid elements
+     */
+    public static SortedMap decorate(SortedMap map, Class keyType, Class valueType) {
+        return new PredicatedSortedMap(
+            map,
+            TypedCollection.getPredicate(keyType),
+            TypedCollection.getPredicate(valueType)
+        );
+    }
+    
+    /**
+     * Restrictive constructor.
+     */
+    protected TypedSortedMap() {
+    }
+
+}
