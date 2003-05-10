d934c4d0f574b1b8f4701c7008b710a9a47f605f
Stephen Colebourne <scolebourne@apache.org>
2003-05-10 15:11:58 +0000

    Add sequenced decorator tests
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131062 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/collections/decorators/TestSequencedSet.java
@@ -0,0 +1,138 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/test/org/apache/commons/collections/decorators/Attic/TestSequencedSet.java,v 1.1 2003/05/10 15:11:58 scolebourne Exp $
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.commons.collections.decorators;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.commons.collections.TestSet;
+
+/**
+ * Extension of {@link TestSet} for exercising the {@link SequencedSet}
+ * implementation.
+ *
+ * @version $Revision: 1.1 $ $Date: 2003/05/10 15:11:58 $
+ * 
+ * @author Henning P. Schmiedehausen
+ * @author Stephen Colebourne
+ */
+public class TestSequencedSet extends TestSet {
+
+    public TestSequencedSet(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(TestSequencedSet.class);
+    }
+
+    public static void main(String args[]) {
+        String[] testCaseName = { TestSequencedSet.class.getName()};
+        junit.textui.TestRunner.main(testCaseName);
+    }
+
+    public Set makeEmptySet() {
+        return SequencedSet.decorate(new HashSet());
+    }
+
+    public Set setupSet() {
+        Set set = makeEmptySet();
+
+        for (int i = 0; i < 10; i++) {
+            set.add(Integer.toString(i));
+        }
+        return set;
+    }
+
+    public void testOrdering() {
+        Set set = setupSet();
+        Iterator it = set.iterator();
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Sequence is wrong", Integer.toString(i), it.next());
+        }
+
+        for (int i = 0; i < 10; i += 2) {
+            assertTrue("Must be able to remove int", set.remove(Integer.toString(i)));
+        }
+
+        it = set.iterator();
+        for (int i = 1; i < 10; i += 2) {
+            assertEquals("Sequence is wrong after remove ", Integer.toString(i), it.next());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            set.add(Integer.toString(i));
+        }
+
+        assertEquals("Size of set is wrong!", 10, set.size());
+
+        it = set.iterator();
+        for (int i = 1; i < 10; i += 2) {
+            assertEquals("Sequence is wrong", Integer.toString(i), it.next());
+        }
+        for (int i = 0; i < 10; i += 2) {
+            assertEquals("Sequence is wrong", Integer.toString(i), it.next());
+        }
+    }
+    
+}
