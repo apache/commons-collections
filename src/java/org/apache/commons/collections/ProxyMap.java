0809aa66f5a532d0a246f306492522475c6a8e23
Daniel Rall <dlr@apache.org>
2003-07-04 03:36:12 +0000

    * collections/src/java/org/apache/commons/collections/ProxyMap.java   Corrected pointer in the header JavaDoc's @deprecated warning to the   better named AbstractMapDecorator which Stephen has placed in the   decorators sub-package.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131106 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/ProxyMap.java
+++ src/java/org/apache/commons/collections/ProxyMap.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/ProxyMap.java,v 1.10 2003/07/04 03:32:03 dlr Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/ProxyMap.java,v 1.11 2003/07/04 03:36:12 dlr Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
@@ -76,10 +76,10 @@ import java.util.Set;
  * implementation it would provide a loophole around the validation. But,
  * you might want that loophole, so this class is kept simple.</p>
  *
- * @deprecated this class has been moved to the deprecated subpackage as
- *  AbstractMapDecorator. Due to be removed in v4.0.
+ * @deprecated This class has been moved to the decorators sub-package
+ * and renamed to AbstractMapDecorator. It will be removed in v4.0.
  * @since Commons Collections 2.0
- * @version $Revision: 1.10 $ $Date: 2003/07/04 03:32:03 $
+ * @version $Revision: 1.11 $ $Date: 2003/07/04 03:36:12 $
  * 
  * @author <a href="mailto:dlr@collab.net">Daniel Rall</a>
  * @author Stephen Colebourne
