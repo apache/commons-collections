e28dab2be37061764d6071a81e58b592b092b005
Stephen Colebourne <scolebourne@apache.org>
2003-09-24 08:23:56 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131188 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/observed/TestObservableSortedSet.java
+++ src/test/org/apache/commons/collections/observed/TestObservableSortedSet.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/test/org/apache/commons/collections/observed/Attic/TestObservableSortedSet.java,v 1.1 2003/09/21 20:01:53 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/test/org/apache/commons/collections/observed/Attic/TestObservableSortedSet.java,v 1.2 2003/09/24 08:23:56 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
@@ -67,11 +67,11 @@ import junit.framework.TestSuite;
 import org.apache.commons.collections.TestSortedSet;
 
 /**
- * Extension of {@link TestSet} for exercising the
- * {@link ObservedSet} implementation.
+ * Extension of {@link TestSortedSet} for exercising the
+ * {@link ObservedSortedSet} implementation.
  *
  * @since Commons Collections 3.0
- * @version $Revision: 1.1 $ $Date: 2003/09/21 20:01:53 $
+ * @version $Revision: 1.2 $ $Date: 2003/09/24 08:23:56 $
  * 
  * @author Stephen Colebourne
  */
