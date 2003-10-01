0b58a2fb6bb3e866d8bdd1fd05fd81472605c754
Stephen Colebourne <scolebourne@apache.org>
2003-10-01 22:36:07 +0000

    Add missing abstract keyword
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131212 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/pairs/AbstractMapEntry.java
+++ src/java/org/apache/commons/collections/pairs/AbstractMapEntry.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/pairs/Attic/AbstractMapEntry.java,v 1.2 2003/09/27 10:33:34 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/pairs/Attic/AbstractMapEntry.java,v 1.3 2003/10/01 22:36:07 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
@@ -63,14 +63,14 @@ import java.util.Map;
  * Abstract Pair class to assist with creating correct Map Entry implementations.
  *
  * @since Commons Collections 3.0
- * @version $Revision: 1.2 $ $Date: 2003/09/27 10:33:34 $
+ * @version $Revision: 1.3 $ $Date: 2003/10/01 22:36:07 $
  * 
  * @author James Strachan
  * @author Michael A. Smith
  * @author Neil O'Toole
  * @author Stephen Colebourne
  */
-public class AbstractMapEntry extends AbstractKeyValue implements Map.Entry {
+public abstract class AbstractMapEntry extends AbstractKeyValue implements Map.Entry {
     
     /**
      * Constructs a new entry with the given key and given value.
