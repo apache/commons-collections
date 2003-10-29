c0968cfcee0cbd37871ffd2f15a2b231df3681dd
Stephen Colebourne <scolebourne@apache.org>
2003-10-29 23:49:18 +0000

    Fix problem where views were being incorrectly created
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131302 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/decorators/UnmodifiableSortedMap.java
+++ src/java/org/apache/commons/collections/decorators/UnmodifiableSortedMap.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/decorators/Attic/UnmodifiableSortedMap.java,v 1.2 2003/08/31 17:24:46 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/decorators/Attic/UnmodifiableSortedMap.java,v 1.3 2003/10/29 23:49:18 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
@@ -66,7 +66,7 @@ import java.util.SortedMap;
  * to ensure it can't be altered.
  *
  * @since Commons Collections 3.0
- * @version $Revision: 1.2 $ $Date: 2003/08/31 17:24:46 $
+ * @version $Revision: 1.3 $ $Date: 2003/10/29 23:49:18 $
  * 
  * @author Stephen Colebourne
  */
@@ -116,17 +116,17 @@ public class UnmodifiableSortedMap extends UnmodifiableMap implements SortedMap 
 
     public SortedMap subMap(Object fromKey, Object toKey) {
         SortedMap map = getSortedMap().subMap(fromKey, toKey);
-        return new UnmodifiableSortedMap(getSortedMap());
+        return new UnmodifiableSortedMap(map);
     }
 
     public SortedMap headMap(Object toKey) {
         SortedMap map = getSortedMap().headMap(toKey);
-        return new UnmodifiableSortedMap(getSortedMap());
+        return new UnmodifiableSortedMap(map);
     }
 
     public SortedMap tailMap(Object fromKey) {
         SortedMap map = getSortedMap().tailMap(fromKey);
-        return new UnmodifiableSortedMap(getSortedMap());
+        return new UnmodifiableSortedMap(map);
     }
 
 }
