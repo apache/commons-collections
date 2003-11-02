37e9c49744ba1dc578e55639514c9799cb8377dd
Stephen Colebourne <scolebourne@apache.org>
2003-11-02 23:40:53 +0000

    Add a default MapIterator implementation that wraps a Map entrySet
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131324 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/collections/iterators/DefaultMapIterator.java
@@ -0,0 +1,227 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/iterators/Attic/DefaultMapIterator.java,v 1.1 2003/11/02 23:40:53 scolebourne Exp $
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowledgement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.commons.collections.iterators;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.collections.pairs.TiedMapEntry;
+
+/**
+ * Implements a <code>MapIterator</code> using a Map entrySet.
+ * <pre>
+ * MapIterator it = map.mapIterator();
+ * while (it.hasNext()) {
+ *   Object key = it.next();
+ *   Object value = it.getValue();
+ *   it.setValue(newValue);
+ * }
+ * </pre>
+ *  
+ * @since Commons Collections 3.0
+ * @version $Revision: 1.1 $ $Date: 2003/11/02 23:40:53 $
+ *
+ * @author Stephen Colebourne
+ */
+public class DefaultMapIterator implements ResetableMapIterator {
+    
+    private final Map map;
+    private Iterator iterator;
+    private Map.Entry last;
+    private boolean canRemove = false;
+    
+    /**
+     * Constructor.
+     * 
+     * @param map  the map to iterate over
+     */
+    public DefaultMapIterator(Map map) {
+        super();
+        this.map = map;
+        this.iterator = map.entrySet().iterator();
+    }
+
+    //-----------------------------------------------------------------------    
+    /**
+     * Checks to see if there are more entries still to be iterated.
+     *
+     * @return <code>true</code> if the iterator has more elements
+     */
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    /**
+     * Gets the next <em>key</em> from the <code>Map</code>.
+     *
+     * @return the next key in the iteration
+     * @throws NoSuchElementException if the iteration is finished
+     */
+    public Object next() {
+        last = (Map.Entry) iterator.next();
+        canRemove = true;
+        return last.getKey();
+    }
+
+    /**
+     * Removes the last returned key from the underlying <code>Map</code>.
+     * <p>
+     * This method can be called once per call to <code>next()</code>.
+     *
+     * @throws UnsupportedOperationException if remove is not supported by the map
+     * @throws IllegalStateException if <code>next()</code> has not yet been called
+     * @throws IllegalStateException if <code>remove()</code> has already been called
+     *  since the last call to <code>next()</code>
+     */
+    public void remove() {
+        if (canRemove == false) {
+            throw new IllegalStateException("Iterator remove() can only be called once after next()");
+        }
+        iterator.remove();
+        last = null;
+        canRemove = false;
+    }
+    
+    //-----------------------------------------------------------------------
+    /**
+     * Gets the current key, which is the key returned by the last call
+     * to <code>next()</code>.
+     *
+     * @return the current key
+     * @throws IllegalStateException if <code>next()</code> has not yet been called
+     */
+    public Object getKey() {
+        if (last == null) {
+            throw new IllegalStateException("Iterator getKey() can only be called after next() and before remove()");
+        }
+        return last.getKey();
+    }
+
+    /**
+     * Gets the current value, which is the value associated with the last key
+     * returned by <code>next()</code>.
+     *
+     * @return the current value
+     * @throws IllegalStateException if <code>next()</code> has not yet been called
+     */
+    public Object getValue() {
+        if (last == null) {
+            throw new IllegalStateException("Iterator getValue() can only be called after next() and before remove()");
+        }
+        return last.getValue();
+    }
+
+    /**
+     * Sets the value associated with the current key.
+     *
+     * @param value  the new value
+     * @return the previous value
+     * @throws UnsupportedOperationException if setValue is not supported by the map
+     * @throws IllegalStateException if <code>next()</code> has not yet been called
+     * @throws IllegalStateException if <code>remove()</code> has been called since the
+     *  last call to <code>next()</code>
+     */
+    public Object setValue(Object value) {
+        if (last == null) {
+            throw new IllegalStateException("Iterator setValue() can only be called after next() and before remove()");
+        }
+        return last.setValue(value);
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Gets the last returned key-value pair from the underlying <code>Map</code>
+     * as a Map Entry instance.
+     * <p>
+     * The returned entry will not change when <code>next</code> is called.
+     * Changes made to the entry via <code>setValue</code> will change the map.
+     * If you call setValue after next on the iterator, a ConcurrentModificationException
+     * may occur.
+     * 
+     * @return the last return key-value pair as an independent Map Entry
+     * @throws IllegalStateException if <code>next()</code> has not yet been called
+     * @throws IllegalStateException if <code>remove()</code> has been called since the
+     *  last call to <code>next()</code>
+     */
+    public Map.Entry asMapEntry() {
+        return new TiedMapEntry(map, getKey());
+    }        
+    
+    /**
+     * Resets the state of the iterator.
+     */
+    public void reset() {
+        iterator = map.entrySet().iterator();
+        last = null;
+        canRemove = false;
+    }
+    
+    /**
+     * Gets the iterator as a String.
+     * 
+     * @return a string version of the iterator
+     */    
+    public String toString() {
+        if (last == null) {
+            return "MapIterator[" + getKey() + "=" + getValue() + "]";
+        } else {
+            return "MapIterator[]";
+        }
+    }
+    
+}
