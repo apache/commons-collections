39a29b486d82a799d8b8552b5c90e753e96568b4
Stephen Colebourne <scolebourne@apache.org>
2003-12-03 11:17:36 +0000

    File layout
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131395 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/list/TestPredicatedList.java
+++ src/test/org/apache/commons/collections/list/TestPredicatedList.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/test/org/apache/commons/collections/list/TestPredicatedList.java,v 1.3 2003/11/18 22:37:14 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/test/org/apache/commons/collections/list/TestPredicatedList.java,v 1.4 2003/12/03 11:17:36 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
@@ -71,11 +71,11 @@ import org.apache.commons.collections.PredicateUtils;
  * {@link PredicatedList} implementation.
  *
  * @since Commons Collections 3.0
- * @version $Revision: 1.3 $ $Date: 2003/11/18 22:37:14 $
+ * @version $Revision: 1.4 $ $Date: 2003/12/03 11:17:36 $
  * 
  * @author Phil Steitz
  */
-public class TestPredicatedList extends AbstractTestList{
+public class TestPredicatedList extends AbstractTestList {
     
     public TestPredicatedList(String testName) {
         super(testName);
