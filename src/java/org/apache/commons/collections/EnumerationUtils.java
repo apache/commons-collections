16c0a94d683a8b96bf6780193ac4dbafc0f612bb
Stephen Colebourne <scolebourne@apache.org>
2004-01-05 21:23:44 +0000

    Add missing since tag Add public constructor
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131518 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/EnumerationUtils.java
+++ src/java/org/apache/commons/collections/EnumerationUtils.java
@@ -1,10 +1,10 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/EnumerationUtils.java,v 1.1 2003/10/28 18:47:47 ggregory Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/EnumerationUtils.java,v 1.2 2004/01/05 21:23:44 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 1999-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 1999-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -65,15 +65,17 @@ import org.apache.commons.collections.iterators.EnumerationIterator;
 /**
  * Provides utility methods for {@link Enumeration} instances.
  * 
+ * @since Commons Collections 3.0
+ * @version $Id: EnumerationUtils.java,v 1.2 2004/01/05 21:23:44 scolebourne Exp $
+ * 
  * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
- * @version $Id: EnumerationUtils.java,v 1.1 2003/10/28 18:47:47 ggregory Exp $
  */
 public class EnumerationUtils {
 
     /**
-     * Do not instantiate.
+     * EnumerationUtils is not normally instantiated.
      */
-    private EnumerationUtils() {
+    public EnumerationUtils() {
         // no init.
     }
     
