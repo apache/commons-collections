e3f754bec1c3cc213543e1b983954f74944f6463
Stephen Colebourne <scolebourne@apache.org>
2004-01-06 22:07:47 +0000

    Update deprecation messages
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131534 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/DoubleOrderedMap.java
+++ src/java/org/apache/commons/collections/DoubleOrderedMap.java
@@ -1,10 +1,10 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/DoubleOrderedMap.java,v 1.10 2003/12/14 12:54:42 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/DoubleOrderedMap.java,v 1.11 2004/01/06 22:07:47 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -136,12 +136,12 @@ import java.util.Set;
  * Collection valuesByValue() returns the values in a Collection whose
  * iterator will iterate over the values in ascending order.
  *
- * @deprecated Use TreeBidiMap in bidimap subpackage. Due to be removed in v4.0.
+ * @deprecated Replaced by TreeBidiMap in bidimap subpackage. Due to be removed in v4.0.
  * @see BidiMap
  * @see org.apache.commons.collections.bidimap.DualTreeBidiMap
  * @see org.apache.commons.collections.bidimap.DualHashBidiMap
  * @since Commons Collections 2.0
- * @version $Revision: 1.10 $ $Date: 2003/12/14 12:54:42 $
+ * @version $Revision: 1.11 $ $Date: 2004/01/06 22:07:47 $
  * 
  * @author Marc Johnson
  */
--- src/java/org/apache/commons/collections/ProxyMap.java
+++ src/java/org/apache/commons/collections/ProxyMap.java
@@ -1,10 +1,10 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/ProxyMap.java,v 1.12 2003/08/31 17:26:44 scolebourne Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//collections/src/java/org/apache/commons/collections/ProxyMap.java,v 1.13 2004/01/06 22:07:47 scolebourne Exp $
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights
+ * Copyright (c) 2002-2004 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -76,12 +76,11 @@ import java.util.Set;
  * implementation it would provide a loophole around the validation. But,
  * you might want that loophole, so this class is kept simple.</p>
  *
- * @deprecated This class has been moved to the decorators sub-package
- * and renamed to AbstractMapDecorator. It will be removed in v4.0.
+ * @deprecated Moved to map subpackage as AbstractMapDecorator. It will be removed in v4.0.
  * @since Commons Collections 2.0
- * @version $Revision: 1.12 $ $Date: 2003/08/31 17:26:44 $
+ * @version $Revision: 1.13 $ $Date: 2004/01/06 22:07:47 $
  * 
- * @author <a href="mailto:dlr@collab.net">Daniel Rall</a>
+ * @author Daniel Rall
  * @author Stephen Colebourne
  */
 public abstract class ProxyMap implements Map {
