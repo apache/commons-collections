fe0a89a0bb44a988bb72c3b2ae14a482506b05f4
Stephen Colebourne <scolebourne@apache.org>
2004-09-22 23:16:48 +0000

    Javadoc tweaks, bug 30191
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@131818 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/OrderedIterator.java
+++ src/java/org/apache/commons/collections/OrderedIterator.java
@@ -18,19 +18,19 @@ package org.apache.commons.collections;
 import java.util.Iterator;
 
 /**
- * Defines an iterator that operates over a ordered collections.
+ * Defines an iterator that operates over an ordered collection.
  * <p>
  * This iterator allows both forward and reverse iteration through the collection.
  *  
  * @since Commons Collections 3.0
- * @version $Revision: 1.4 $ $Date: 2004/02/18 01:15:42 $
+ * @version $Revision: 1.5 $ $Date: 2004/09/22 23:16:48 $
  *
  * @author Stephen Colebourne
  */
 public interface OrderedIterator extends Iterator {
-    
+
     /**
-     * Checks to see if there is a previous entry that can be iterated to.
+     * Checks to see if there is a previous element that can be iterated to.
      *
      * @return <code>true</code> if the iterator has a previous element
      */
@@ -39,7 +39,7 @@ public interface OrderedIterator extends Iterator {
     /**
      * Gets the previous element from the collection.
      *
-     * @return the previous key in the iteration
+     * @return the previous element in the iteration
      * @throws java.util.NoSuchElementException if the iteration is finished
      */
     Object previous();
