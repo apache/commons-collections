4c282328c8462b49b8773481251abbd8443f46dc
Stephen Colebourne <scolebourne@apache.org>
2005-03-22 22:53:50 +0000

    Add DefaultedMap that returns a default value if the key is not in the map RFE 30911, by Rafael U. C. Afonso
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/collections/trunk@158692 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/map/TestAll.java
+++ src/test/org/apache/commons/collections/map/TestAll.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2003-2004 The Apache Software Foundation
+ *  Copyright 2003-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ public class TestAll extends TestCase {
         
         suite.addTest(TestCaseInsensitiveMap.suite());
         suite.addTest(TestCompositeMap.suite());
+        suite.addTest(TestDefaultedMap.suite());
         suite.addTest(TestFlat3Map.suite());
         suite.addTest(TestHashedMap.suite());
         suite.addTest(TestIdentityMap.suite());
