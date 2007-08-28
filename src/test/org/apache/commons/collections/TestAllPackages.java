683959a47192c61fac6f2aa7c415c477cb23ea67
Stephen Kestle <skestle@apache.org>
2007-08-28 12:08:29 +0000

    Generified InstantiateFactory (missed -another;sorry- commit file!)
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/branches/collections_jdk5_branch@570395 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/TestAllPackages.java
+++ src/test/org/apache/commons/collections/TestAllPackages.java
@@ -16,6 +16,10 @@
  */
 package org.apache.commons.collections;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -27,30 +31,17 @@ import junit.framework.TestSuite;
  * 
  * @author Stephen Colebourne
  */
-public class TestAllPackages extends TestCase {
-    public TestAllPackages(String testName) {
-        super(testName);
-    }
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(org.apache.commons.collections.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.bag.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.bidimap.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.buffer.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.collection.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.comparators.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.iterators.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.keyvalue.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.list.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.map.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.set.TestAll.suite());
-        return suite;
-    }
-        
-    public static void main(String args[]) {
-        String[] testCaseName = { TestAllPackages.class.getName() };
-        junit.textui.TestRunner.main(testCaseName);
-    }
-    
+@RunWith(Suite.class)
+@SuiteClasses({org.apache.commons.collections.TestAll.class,
+        org.apache.commons.collections.bag.TestAll.class,
+        org.apache.commons.collections.bidimap.TestAll.class,
+        org.apache.commons.collections.buffer.TestAll.class,
+        org.apache.commons.collections.collection.TestAll.class,
+        org.apache.commons.collections.comparators.TestAll.class,
+        org.apache.commons.collections.iterators.TestAll.class,
+        org.apache.commons.collections.keyvalue.TestAll.class,
+        org.apache.commons.collections.list.TestAll.class,
+        org.apache.commons.collections.map.TestAll.class,
+        org.apache.commons.collections.set.TestAll.class})
+public class TestAllPackages {
 }
