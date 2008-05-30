d887c5913fddcecad8483ec9a0cbf407ed08c157
Henri Yandell <bayard@apache.org>
2008-05-30 06:23:45 +0000

    Applying Joerg's final patch from COLLECTIONS-266, including the unit test that shows the problem and fixes the problem by making the hashcode transient, and moving the hashcode implementation such that it can be called from the deserialization as well as the hashcode method
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@661577 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/keyvalue/MultiKey.java
+++ src/java/org/apache/commons/collections/keyvalue/MultiKey.java
@@ -16,6 +16,8 @@
  */
 package org.apache.commons.collections.keyvalue;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Arrays;
 
@@ -54,7 +56,7 @@ public class MultiKey implements Serializable {
     /** The individual keys */
     private final Object[] keys;
     /** The cached hashCode */
-    private final int hashCode;
+    private transient int hashCode;
     
     /**
      * Constructor taking two keys.
@@ -164,13 +166,7 @@ public class MultiKey implements Serializable {
             this.keys = keys;
         }
         
-        int total = 0;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] != null) {
-                total ^= keys[i].hashCode();
-            }
-        }
-        hashCode = total;
+        calculateHashCode(keys);
     }
     
     //-----------------------------------------------------------------------
@@ -255,4 +251,29 @@ public class MultiKey implements Serializable {
         return "MultiKey" + Arrays.asList(keys).toString();
     }
 
+	/**
+	 * Calculate the hash code of the instance using the provided keys.
+	 * @param keys
+	 */
+	private void calculateHashCode(Object[] keys)
+	{
+		int total = 0;
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i] != null) {
+                total ^= keys[i].hashCode();
+            }
+        }
+        hashCode = total;
+	}
+	
+	/**
+	 * Recalculate the hash code after deserialization. The hash code of some
+	 * keys might have change (hash codes based on the system hash code are
+	 * only stable for the same process). 
+	 * @return the instance with recalculated hash code
+	 */
+	private Object readResolve() {
+		calculateHashCode(keys);
+		return this;
+	}
 }
