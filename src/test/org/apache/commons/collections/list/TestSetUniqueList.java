0122245f02ba7b22dd40f38c98aa2f08984707bd
Henri Yandell <bayard@apache.org>
2008-11-05 15:47:43 +0000

    Applying Bjorn Townsend's unit test and my fix for COLLECTIONS-304 - fixing SetUniqueList so the set method doesn't let the uniqueness get out of sync
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@711591 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/list/TestSetUniqueList.java
+++ src/test/org/apache/commons/collections/list/TestSetUniqueList.java
@@ -443,4 +443,31 @@ public class TestSetUniqueList extends AbstractTestList {
 //        writeExternalFormToDisk((java.io.Serializable) collection, "D:/dev/collections/data/test/SetUniqueList.fullCollection.version3.1.obj");
 //    }
 
+    public void testCollections304() {
+        List list = new LinkedList();
+        SetUniqueList decoratedList = SetUniqueList.decorate(list);
+        String s1 = "Apple";
+        String s2 = "Lemon";
+        String s3 = "Orange";
+        String s4 = "Strawberry";
+
+        decoratedList.add(s1);
+        decoratedList.add(s2);
+        decoratedList.add(s3);
+
+        assertEquals(3, decoratedList.size());
+
+        decoratedList.set(1, s4);
+
+        assertEquals(3, decoratedList.size());
+
+        decoratedList.add(1, s4);
+
+        assertEquals(3, decoratedList.size());
+
+        decoratedList.add(1, s2);
+
+        assertEquals(4, decoratedList.size());
+    }
+
 }
