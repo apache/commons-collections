3d5f3fec054fdafb33f2eb2cc4f27109929e6508
Matthew Jason Benson <mbenson@apache.org>
2009-03-09 21:45:53 +0000

    generics
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/branches/collections_jdk5_branch@751858 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/map/TestTransformedMap.java
+++ src/test/org/apache/commons/collections/map/TestTransformedMap.java
@@ -94,9 +94,9 @@ public class TestTransformedMap<K, V> extends AbstractTestIterableMap<K, V> {
 
         assertEquals(new Integer((String) els[0]), map.remove(els[0]));
 
-        Set entrySet = map.entrySet();
-        Map.Entry[] array = (Map.Entry[]) entrySet.toArray(new Map.Entry[0]);
-        array[0].setValue("66");
+        Set<Map.Entry<K, V>> entrySet = map.entrySet();
+        Map.Entry<K, V>[] array = entrySet.toArray(new Map.Entry[0]);
+        array[0].setValue((V) "66");
         assertEquals(new Integer(66), array[0].getValue());
         assertEquals(new Integer(66), map.get(array[0].getKey()));
 
