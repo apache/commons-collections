e3984b95bd6d70ecca4dbf675c9dd71d55aec356
Henri Yandell <bayard@apache.org>
2009-09-15 05:57:56 +0000

    Merging from -r468106:814127 of collections_jdk5_branch - namely where this code was generified; mostly in r738956.
    Also see the following revisions:
    
        ------------------------------------------------------------------------
        r751894 | mbenson | 2009-03-09 15:48:07 -0700 (Mon, 09 Mar 2009) | 1 line
        
        add splitmap package whose original goal is to provide a more versatile TransformedMap implementation
        ------------------------------------------------------------------------
        r643795 | skestle | 2008-04-02 01:49:57 -0700 (Wed, 02 Apr 2008) | 5 lines
        
        Generified EqualPredicate and created individual test class moved from TestPredicateUtils
        
        Added assertFalse() and assertTrue to BasicPredicateTestBase with (Predicate, Object) parameters
        
        Issues: COLLECTIONS-243, COLLECTIONS-253, COLLECTIONS-293
        ------------------------------------------------------------------------
        r572157 | skestle | 2007-09-02 14:12:28 -0700 (Sun, 02 Sep 2007) | 1 line
        
        Cleaned imports, formatting
        ------------------------------------------------------------------------
        r570395 | skestle | 2007-08-28 05:08:29 -0700 (Tue, 28 Aug 2007) | 1 line
        
        Generified InstantiateFactory (missed -another;sorry- commit file!)
        ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@815143 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/TestAllPackages.java
+++ src/test/org/apache/commons/collections/TestAllPackages.java
@@ -16,9 +16,9 @@
  */
 package org.apache.commons.collections;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
 /**
  * Entry point for all Collections project tests.
@@ -26,31 +26,23 @@ import junit.framework.TestSuite;
  * @version $Revision$ $Date$
  *
  * @author Stephen Colebourne
+ * @author Stephen Kestle
  */
-public class TestAllPackages extends TestCase {
-    public TestAllPackages(String testName) {
-        super(testName);
-    }
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
-        suite.addTest(org.apache.commons.collections.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.bag.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.bidimap.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.buffer.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.collection.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.comparators.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.iterators.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.keyvalue.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.list.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.map.TestAll.suite());
-        suite.addTest(org.apache.commons.collections.set.TestAll.suite());
-        return suite;
-    }
-        
-    public static void main(String args[]) {
-        String[] testCaseName = { TestAllPackages.class.getName() };
-        junit.textui.TestRunner.main(testCaseName);
-    }
-    
+@RunWith(Suite.class)
+@SuiteClasses({
+    org.apache.commons.collections.TestAll.class,
+    org.apache.commons.collections.bag.TestAll.class,
+    org.apache.commons.collections.bidimap.TestAll.class,
+    org.apache.commons.collections.buffer.TestAll.class,
+    org.apache.commons.collections.collection.TestAll.class,
+    org.apache.commons.collections.comparators.TestAll.class,
+    org.apache.commons.collections.functors.TestAll.class,
+    org.apache.commons.collections.iterators.TestAll.class,
+    org.apache.commons.collections.keyvalue.TestAll.class,
+    org.apache.commons.collections.list.TestAll.class,
+    org.apache.commons.collections.map.TestAll.class,
+    org.apache.commons.collections.set.TestAll.class,
+    org.apache.commons.collections.splitmap.TestAll.class
+})
+public class TestAllPackages {
 }
