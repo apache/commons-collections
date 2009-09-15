a9447a7c7213f40318a0f3f0ca78da9d56654d69
Henri Yandell <bayard@apache.org>
2009-09-15 05:55:57 +0000

    Merging from -r468106:814127 of collections_jdk5_branch - namely where this code was generified; mostly in r738956.
    Also see the following revisions:
    
        ------------------------------------------------------------------------
        r751871 | mbenson | 2009-03-09 15:13:34 -0700 (Mon, 09 Mar 2009) | 1 line
        
        Add OrderedMap to our SortedMap implementations
        ------------------------------------------------------------------------
        r471189 | scolebourne | 2006-11-04 05:57:57 -0800 (Sat, 04 Nov 2006) | 1 line
        
        Remove getMap(), getOrderedMap() and getSortedMap() - use decorated()
        ------------------------------------------------------------------------
        r471180 | scolebourne | 2006-11-04 05:27:44 -0800 (Sat, 04 Nov 2006) | 1 line
        
        Abstract*Decorator - Generify and use covariant return types
        ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@815073 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/map/AbstractSortedMapDecorator.java
+++ src/java/org/apache/commons/collections/map/AbstractSortedMapDecorator.java
@@ -17,7 +17,15 @@
 package org.apache.commons.collections.map;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
+
+import org.apache.commons.collections.IterableSortedMap;
+import org.apache.commons.collections.OrderedMapIterator;
+import org.apache.commons.collections.iterators.ListIteratorWrapper;
 
 /** 
  * Provides a base decorator that enables additional functionality to be added
@@ -31,13 +39,15 @@ import java.util.SortedMap;
  * it would provide a loophole around the validation.
  * But, you might want that loophole, so this class is kept simple.
  *
+ * @param <K> the type of the keys in the map
+ * @param <V> the type of the values in the map
  * @since Commons Collections 3.0
  * @version $Revision$ $Date$
  *
  * @author Stephen Colebourne
  */
-public abstract class AbstractSortedMapDecorator
-        extends AbstractMapDecorator implements SortedMap {
+public abstract class AbstractSortedMapDecorator<K, V> extends AbstractMapDecorator<K, V> implements
+        IterableSortedMap<K, V> {
 
     /**
      * Constructor only used in deserialization, do not use otherwise.
@@ -53,7 +63,7 @@ public abstract class AbstractSortedMapDecorator
      * @param map  the map to decorate, must not be null
      * @throws IllegalArgumentException if the collection is null
      */
-    public AbstractSortedMapDecorator(SortedMap map) {
+    public AbstractSortedMapDecorator(SortedMap<K, V> map) {
         super(map);
     }
 
@@ -62,33 +72,92 @@ public abstract class AbstractSortedMapDecorator
      * 
      * @return the decorated map
      */
-    protected SortedMap getSortedMap() {
-        return (SortedMap) map;
+    protected SortedMap<K, V> decorated() {
+        return (SortedMap<K, V>) super.decorated();
     }
 
     //-----------------------------------------------------------------------
-    public Comparator comparator() {
-        return getSortedMap().comparator();
+    public Comparator<? super K> comparator() {
+        return decorated().comparator();
     }
 
-    public Object firstKey() {
-        return getSortedMap().firstKey();
+    public K firstKey() {
+        return decorated().firstKey();
     }
 
-    public SortedMap headMap(Object toKey) {
-        return getSortedMap().headMap(toKey);
+    public K lastKey() {
+        return decorated().lastKey();
     }
 
-    public Object lastKey() {
-        return getSortedMap().lastKey();
+    public SortedMap<K, V> subMap(K fromKey, K toKey) {
+        return decorated().subMap(fromKey, toKey);
     }
 
-    public SortedMap subMap(Object fromKey, Object toKey) {
-        return getSortedMap().subMap(fromKey, toKey);
+    public SortedMap<K, V> headMap(K toKey) {
+        return decorated().headMap(toKey);
     }
 
-    public SortedMap tailMap(Object fromKey) {
-        return getSortedMap().tailMap(fromKey);
+    public SortedMap<K, V> tailMap(K fromKey) {
+        return decorated().tailMap(fromKey);
     }
 
+    public K previousKey(K key) {
+        SortedMap<K, V> headMap = headMap(key);
+        return headMap.isEmpty() ? null : headMap.lastKey();
+    };
+
+    public K nextKey(K key) {
+        Iterator<K> it = tailMap(key).keySet().iterator();
+        it.next();
+        return it.hasNext() ? it.next() : null;
+    };
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public OrderedMapIterator<K, V> mapIterator() {
+        return new SortedMapIterator<K, V>(entrySet());
+    }
+
+    /**
+     * OrderedMapIterator implementation.
+     *
+     * @param <K>
+     * @param <V>
+     */
+    protected static class SortedMapIterator<K, V> extends EntrySetToMapIteratorAdapter<K, V>
+            implements OrderedMapIterator<K, V> {
+
+        /**
+         * Create a new AbstractSortedMapDecorator.SortedMapIterator.
+         */
+        protected SortedMapIterator(Set<Map.Entry<K, V>> entrySet) {
+            super(entrySet);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public synchronized void reset() {
+            super.reset();
+            iterator = new ListIteratorWrapper<Map.Entry<K, V>>(iterator);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public boolean hasPrevious() {
+            return ((ListIterator<Map.Entry<K, V>>) iterator).hasPrevious();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public K previous() {
+            entry = ((ListIterator<Map.Entry<K, V>>) iterator).previous();
+            return getKey();
+        }
+    }
 }
