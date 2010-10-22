b5e8745a419f27eff18a906bf3acceca6d6eb792
Sebastian Bazley <sebb@apache.org>
2010-10-22 10:37:09 +0000

    A few raw types
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@1026283 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/map/TestMultiValueMap.java
+++ src/test/org/apache/commons/collections/map/TestMultiValueMap.java
@@ -399,15 +399,15 @@ public class TestMultiValueMap<K, V> extends AbstractTestObject {
 //    }
 
     public void testEmptyMapCompatibility() throws Exception {
-        Map map = makeEmptyMap();
-        Map map2 = (Map) readExternalFormFromDisk(getCanonicalEmptyCollectionName(map));
+        Map<?,?> map = makeEmptyMap();
+        Map<?,?> map2 = (Map<?,?>) readExternalFormFromDisk(getCanonicalEmptyCollectionName(map));
         assertEquals("Map is empty", 0, map2.size());
     }
     public void testFullMapCompatibility() throws Exception {
-        Map map = (Map) makeObject();
-        Map map2 = (Map) readExternalFormFromDisk(getCanonicalFullCollectionName(map));
+        Map<?,?> map = (Map<?,?>) makeObject();
+        Map<?,?> map2 = (Map<?,?>) readExternalFormFromDisk(getCanonicalFullCollectionName(map));
         assertEquals("Map is the right size", map.size(), map2.size());
-        for (Iterator it = map.keySet().iterator(); it.hasNext();) {
+        for (Iterator<?> it = map.keySet().iterator(); it.hasNext();) {
             Object key = it.next();
             assertEquals( "Map had inequal elements", map.get(key), map2.get(key) );
             map2.remove(key);
