b6aac639d99f2900f9b542455ab9f1142ae50121
Thomas Neidhart <tn@apache.org>
2012-06-23 16:29:39 +0000

    Fixed javadoc (closing paragraph elements).
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@1353156 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/collections/Closure.java
+++ src/main/java/org/apache/commons/collections/Closure.java
@@ -20,10 +20,10 @@ package org.apache.commons.collections;
  * Defines a functor interface implemented by classes that do something.
  * <p>
  * A <code>Closure</code> represents a block of code which is executed from
- * inside some block, function or iteration. It operates an input object.
+ * inside some block, function or iteration. It operates an input object.</p>
  * <p>
  * Standard implementations of common closures are provided by
- * {@link ClosureUtils}. These include method invokation and for/while loops.
+ * {@link ClosureUtils}. These include method invocation and for/while loops.</p>
  *
  * @param <T> the type that the closure acts on
  * @since Commons Collections 1.0
