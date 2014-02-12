2ddd54a7ddfead528f2b1de92dc8dd16be7f5f89
Thomas Neidhart <tn@apache.org>
2014-02-12 21:17:41 +0000

    [COLLECTIONS-507] Removed wrong type bounds for ComparatorUtils.chainedComparator. Thanks to Gerson.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@1567759 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/collections4/ComparatorUtils.java
+++ src/main/java/org/apache/commons/collections4/ComparatorUtils.java
@@ -74,9 +74,7 @@ public class ComparatorUtils {
      * @throws NullPointerException if comparators array is null or contains a null
      * @see ComparatorChain
      */
-    public static <E extends Comparable<? super E>> Comparator<E> chainedComparator(
-            final Comparator<E>... comparators) {
-
+    public static <E> Comparator<E> chainedComparator(final Comparator<E>... comparators) {
         final ComparatorChain<E> chain = new ComparatorChain<E>();
         for (final Comparator<E> comparator : comparators) {
             if (comparator == null) {
@@ -100,9 +98,7 @@ public class ComparatorUtils {
      * @see ComparatorChain
      */
     @SuppressWarnings("unchecked")
-    public static <E extends Comparable<? super E>> Comparator<E> chainedComparator(
-            final Collection<Comparator<E>> comparators) {
-
+    public static <E> Comparator<E> chainedComparator(final Collection<Comparator<E>> comparators) {
         return chainedComparator(
             (Comparator<E>[]) comparators.toArray(new Comparator[comparators.size()])
         );
