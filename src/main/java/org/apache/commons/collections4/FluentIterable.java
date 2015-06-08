bb7ddb5396d05f0baa1ad9c5300af045e654f05e
Thomas Neidhart <tn@apache.org>
2015-06-08 20:06:29 +0000

    Add missing svn:keywords.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/trunk@1684264 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/collections4/FluentIterable.java
+++ src/main/java/org/apache/commons/collections4/FluentIterable.java
@@ -61,7 +61,7 @@ import org.apache.commons.collections4.iterators.SingletonIterator;
  *
  * @param <E>  the element type
  * @since 4.1
- * @version $Id: $
+ * @version $Id$
  */
 public class FluentIterable<E> implements Iterable<E> {
 
