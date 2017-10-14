d4137820b1116c98659d643f7cf45255abeded21
Kavuri, Vamsi <vamsi.kavuri@capitalone.com>
2017-10-13 22:53:27 -0400

    update

--- src/test/java/org/apache/commons/collections4/MapUtilsTest.java
+++ src/test/java/org/apache/commons/collections4/MapUtilsTest.java
@@ -1068,7 +1068,7 @@ public class MapUtilsTest {
         in.put("key", "str");
         
         assertEquals("str", MapUtils.getString(in,"key", "defualt"));
-        assertEquals("str", MapUtils.getString(in,"key"), "str");
+        assertEquals("str", MapUtils.getString(in,"key"));
         assertEquals(null, MapUtils.getString(null,"key"));
         assertEquals("default", MapUtils.getString(in,"noKey", "default"));
         assertEquals("default", MapUtils.getString(null,"noKey", "default"));
