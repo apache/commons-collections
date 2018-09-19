23747116069c150e26e0dd42d61331b1108e1e01
Maxim Solodovnik <solomax666@gmail.com>
2018-09-19 09:09:10 -0600

    [COLLECTIONS-696] AbstractReferenceMap made easier for subclassing; PR #51.

--- src/main/java/org/apache/commons/collections4/map/AbstractReferenceMap.java
+++ src/main/java/org/apache/commons/collections4/map/AbstractReferenceMap.java
@@ -400,13 +400,15 @@ public abstract class AbstractReferenceMap<K, V> extends AbstractHashedMap<K, V>
         HashEntry<K, V> previous = null;
         HashEntry<K, V> entry = data[index];
         while (entry != null) {
-            if (((ReferenceEntry<K, V>) entry).purge(ref)) {
+            ReferenceEntry<K, V> refEntry = (ReferenceEntry<K, V>) entry;
+            if (refEntry.purge(ref)) {
                 if (previous == null) {
                     data[index] = entry.next;
                 } else {
                     previous.next = entry.next;
                 }
                 this.size--;
+                refEntry.onPurge();
                 return;
             }
             previous = entry;
@@ -722,11 +724,17 @@ public abstract class AbstractReferenceMap<K, V> extends AbstractHashedMap<K, V>
         }
 
         /**
+         * This is the callback for custom "after purge" logic
+         */
+        protected void onPurge() {
+        }
+
+        /**
          * Purges the specified reference
          * @param ref  the reference to purge
          * @return true or false
          */
-        boolean purge(final Reference<?> ref) {
+        protected boolean purge(final Reference<?> ref) {
             boolean r = parent.keyType != ReferenceStrength.HARD && key == ref;
             r = r || parent.valueType != ReferenceStrength.HARD && value == ref;
             if (r) {
@@ -736,7 +744,7 @@ public abstract class AbstractReferenceMap<K, V> extends AbstractHashedMap<K, V>
                 if (parent.valueType != ReferenceStrength.HARD) {
                     ((Reference<?>) value).clear();
                 } else if (parent.purgeValues) {
-                    value = null;
+                    nullValue();
                 }
             }
             return r;
@@ -749,6 +757,13 @@ public abstract class AbstractReferenceMap<K, V> extends AbstractHashedMap<K, V>
          */
         protected ReferenceEntry<K, V> next() {
             return (ReferenceEntry<K, V>) next;
+        }
+
+        /**
+         * This method can be overriden to provide custom logic to purge value
+         */
+        protected void nullValue() {
+            value = null;
         }
     }
 
@@ -1072,5 +1087,14 @@ public abstract class AbstractReferenceMap<K, V> extends AbstractHashedMap<K, V>
      */
     protected boolean isKeyType(final ReferenceStrength type) {
         return this.keyType == type;
+    }
+
+    /**
+     * Provided protected read-only access to the value type.
+     * @param type the type to check against.
+     * @return true if valueType has the specified type
+     */
+    protected boolean isValueType(final ReferenceStrength type) {
+        return this.valueType == type;
     }
 }
