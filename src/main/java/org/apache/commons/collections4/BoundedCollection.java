d8fd53176635bf531184bbb16701d779e096b490
Gary Gregory <garydgregory@gmail.com>
2018-11-01 14:10:30 -0600

    Javadoc nits.

--- src/main/java/org/apache/commons/collections4/BoundedCollection.java
+++ src/main/java/org/apache/commons/collections4/BoundedCollection.java
@@ -36,14 +36,14 @@ public interface BoundedCollection<E> extends Collection<E> {
     /**
      * Returns true if this collection is full and no new elements can be added.
      *
-     * @return <code>true</code> if the collection is full
+     * @return <code>true</code> if the collection is full.
      */
     boolean isFull();
 
     /**
      * Gets the maximum size of the collection (the bound).
      *
-     * @return the maximum number of elements the collection can hold
+     * @return the maximum number of elements the collection can hold.
      */
     int maxSize();
 
