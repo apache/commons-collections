a2b8f33229be2da04425e39e84bf594297b1d0d2
Dennis Goermann <d.goermann@gmx.de>
2019-05-22 18:40:49 +0200

    COLLETION-699 spaces instead of tabs
    spaces instead of tabs

--- src/main/java/org/apache/commons/collections4/iterators/PairingIterator.java
+++ src/main/java/org/apache/commons/collections4/iterators/PairingIterator.java
@@ -47,120 +47,120 @@ import org.apache.commons.collections4.iterators.PairingIterator.Entry;
  */
 public class PairingIterator<F, S> implements Iterator<Entry<F, S>> {
 
-	private final Iterator<F> firstIterator;
-	private final Iterator<S> secondIterator;
+    private final Iterator<F> firstIterator;
+    private final Iterator<S> secondIterator;
 
-	/**
-	 * Constructs a new <code>PairingIterator</code> that will provide a
-	 * {@link Entry} containing the child iterator elements.
-	 * 
-	 * @param firstIterator  the first iterator
-	 * @param secondIterator the second iterator
-	 */
-	public PairingIterator(Iterator<F> firstIterator, Iterator<S> secondIterator) {
-		this.firstIterator = firstIterator;
-		this.secondIterator = secondIterator;
-	}
+    /**
+     * Constructs a new <code>PairingIterator</code> that will provide a
+     * {@link Entry} containing the child iterator elements.
+     * 
+     * @param firstIterator  the first iterator
+     * @param secondIterator the second iterator
+     */
+    public PairingIterator(Iterator<F> firstIterator, Iterator<S> secondIterator) {
+        this.firstIterator = firstIterator;
+        this.secondIterator = secondIterator;
+    }
 
-	/**
-	 * Returns {@code true} if one of the child iterators has remaining elements.
-	 */
-	@Override
-	public boolean hasNext() {
-		return (null != firstIterator && firstIterator.hasNext())
-				|| (null != secondIterator && secondIterator.hasNext());
-	}
+    /**
+     * Returns {@code true} if one of the child iterators has remaining elements.
+     */
+    @Override
+    public boolean hasNext() {
+        return (null != firstIterator && firstIterator.hasNext())
+                || (null != secondIterator && secondIterator.hasNext());
+    }
 
-	/**
-	 * Returns the next {@link Entry} with the next values of the child iterators.
-	 * 
-	 * @return a {@link Entry} with the next values of child iterators
-	 * @throws NoSuchElementException if no child iterator has any more elements
-	 */
-	@Override
-	public Entry<F, S> next() throws NoSuchElementException {
-		if (!hasNext()) {
-			throw new NoSuchElementException();
-		}
-		final F firstValue = null != firstIterator && firstIterator.hasNext() ? firstIterator.next() : null;
-		final S secondValue = null != secondIterator && secondIterator.hasNext() ? secondIterator.next() : null;
-		return new Entry<F, S>(firstValue, secondValue);
-	}
+    /**
+     * Returns the next {@link Entry} with the next values of the child iterators.
+     * 
+     * @return a {@link Entry} with the next values of child iterators
+     * @throws NoSuchElementException if no child iterator has any more elements
+     */
+    @Override
+    public Entry<F, S> next() throws NoSuchElementException {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        final F firstValue = null != firstIterator && firstIterator.hasNext() ? firstIterator.next() : null;
+        final S secondValue = null != secondIterator && secondIterator.hasNext() ? secondIterator.next() : null;
+        return new Entry<F, S>(firstValue, secondValue);
+    }
 
-	/**
-	 * Removes the last returned values of the child iterators.
-	 */
-	@Override
-	public void remove() {
-		if (firstIterator != null) {
-			firstIterator.remove();
-		}
-		if (secondIterator != null) {
-			secondIterator.remove();
-		}
-	}
+    /**
+     * Removes the last returned values of the child iterators.
+     */
+    @Override
+    public void remove() {
+        if (firstIterator != null) {
+            firstIterator.remove();
+        }
+        if (secondIterator != null) {
+            secondIterator.remove();
+        }
+    }
 
-	/**
-	 * Contains two values of different generic types.
-	 *
-	 * @param <F> the type of the first element
-	 * @param <S> the type of the second element
-	 */
-	public static class Entry<F, S> {
-		private final F firstValue;
-		private final S secondValue;
+    /**
+     * Contains two values of different generic types.
+     *
+     * @param <F> the type of the first element
+     * @param <S> the type of the second element
+     */
+    public static class Entry<F, S> {
+        private final F firstValue;
+        private final S secondValue;
 
-		/**
-		 * Constructs a new {@link Entry} of two generic values.
-		 * 
-		 * @param firstValue  the first value
-		 * @param secondValue the second value
-		 */
-		Entry(F firstValue, S secondValue) {
-			this.firstValue = firstValue;
-			this.secondValue = secondValue;
-		}
+        /**
+         * Constructs a new {@link Entry} of two generic values.
+         * 
+         * @param firstValue  the first value
+         * @param secondValue the second value
+         */
+        Entry(F firstValue, S secondValue) {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+        }
 
-		/**
-		 * Returns the first value.
-		 * 
-		 * @return the first value
-		 */
-		public F getFirstValue() {
-			return firstValue;
-		}
+        /**
+         * Returns the first value.
+         * 
+         * @return the first value
+         */
+        public F getFirstValue() {
+            return firstValue;
+        }
 
-		/**
-		 * Returns the second value.
-		 * 
-		 * @return the second value
-		 */
-		public S getSecondValue() {
-			return secondValue;
-		}
+        /**
+         * Returns the second value.
+         * 
+         * @return the second value
+         */
+        public S getSecondValue() {
+            return secondValue;
+        }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(firstValue, secondValue);
-		}
+        @Override
+        public int hashCode() {
+            return Objects.hash(firstValue, secondValue);
+        }
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Entry other = (Entry) obj;
-			return Objects.equals(firstValue, other.firstValue) && Objects.equals(secondValue, other.secondValue);
-		}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Entry other = (Entry) obj;
+            return Objects.equals(firstValue, other.firstValue) && Objects.equals(secondValue, other.secondValue);
+        }
 
-		@Override
-		public String toString() {
-			return "Entry [firstValue=" + firstValue + ", secondValue=" + secondValue + "]";
-		}
+        @Override
+        public String toString() {
+            return "Entry [firstValue=" + firstValue + ", secondValue=" + secondValue + "]";
+        }
 
-	}
+    }
 
 }
