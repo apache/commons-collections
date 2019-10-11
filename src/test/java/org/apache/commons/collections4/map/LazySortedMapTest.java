297e7300529ae48ec437672b913a02c7b18b6519
Trey Woodlief <adwiii7@gmail.com>
2019-10-11 15:24:37 -0400

    Added additional tests for map classes based on mutation results

--- src/test/java/org/apache/commons/collections4/map/LazySortedMapTest.java
+++ src/test/java/org/apache/commons/collections4/map/LazySortedMapTest.java
@@ -22,6 +22,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.FactoryUtils;
@@ -38,7 +39,16 @@ import org.junit.Test;
 @SuppressWarnings("boxing")
 public class LazySortedMapTest<K, V> extends AbstractSortedMapTest<K, V> {
 
+	private class ReverseStringComparator implements Comparator<String> {
+		@Override
+		public int compare(String arg0, String arg1) {
+			return arg1.compareTo(arg0);
+		}
+	}
+	
     private static final Factory<Integer> oneFactory = FactoryUtils.constantFactory(1);
+    
+    protected final Comparator<String> reverseStringComparator = new ReverseStringComparator();
 
     public LazySortedMapTest(final String testName) {
         super(testName);
@@ -94,6 +104,25 @@ public class LazySortedMapTest<K, V> extends AbstractSortedMapTest<K, V> {
         final Comparator<?> c = map.comparator();
         assertTrue("natural order, so comparator should be null",
             c == null);
+    }
+    
+    public void testReverseSortOrder() {
+        final SortedMap<String, Number> map = lazySortedMap(new ConcurrentSkipListMap<String, Number>(reverseStringComparator), oneFactory);
+        map.put("A",  5);
+        map.get("B"); // Entry with value "One" created
+        map.put("C", 8);
+        assertEquals("Last key should be A", "A", map.lastKey());
+        assertEquals("First key should be C", "C", map.firstKey());
+        assertEquals("First key in tail map should be B",
+            "B", map.tailMap("B").firstKey());
+        assertEquals("Last key in head map should be B",
+            "B", map.headMap("A").lastKey());
+        assertEquals("Last key in submap should be B",
+            "B", map.subMap("C","A").lastKey());
+
+        final Comparator<?> c = map.comparator();
+        assertTrue("natural order, so comparator should be null",
+            c == reverseStringComparator);
     }
 
     public void testTransformerDecorate() {
