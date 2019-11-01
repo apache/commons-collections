c156c54c731d84afcf2b8aa747223fceaba2a74b
data17 <chenguopingdota@163.com>
2019-11-01 17:51:17 +0800

    Add a test case in AbstractMultiValuedMapTest for AbstractMultiValuedMapTest.MultiValuedMapIterator

--- src/test/java/org/apache/commons/collections4/multimap/HashSetValuedHashMapTest.java
+++ src/test/java/org/apache/commons/collections4/multimap/HashSetValuedHashMapTest.java
@@ -51,6 +51,11 @@ public class HashSetValuedHashMapTest<K, V> extends AbstractMultiValuedMapTest<K
         return new HashSetValuedHashMap<>();
     }
 
+    @Override
+    public boolean isHashSetValue() {
+        return true;
+    }
+
     // -----------------------------------------------------------------------
     @SuppressWarnings("unchecked")
     public void testSetValuedMapAdd() {
