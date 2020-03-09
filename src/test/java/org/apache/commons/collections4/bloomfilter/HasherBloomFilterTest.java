c18cd7b86ef088d0591b395b71fc1d521148b5c2
Alex Herbert <aherbert@apache.org>
2020-03-09 22:07:09 +0000

    Increase HasherBloomFilter test coverage.
    Coverage cannot reach 100% because assert statements have been included
    that test assumptions. These asserts are unreachable if the StaticHasher
    functions as expected and returns an iterator with at least 1 value when
    it reports a non-zero size.

--- src/test/java/org/apache/commons/collections4/bloomfilter/HasherBloomFilterTest.java
+++ src/test/java/org/apache/commons/collections4/bloomfilter/HasherBloomFilterTest.java
@@ -19,10 +19,15 @@ package org.apache.commons.collections4.bloomfilter;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.commons.collections4.bloomfilter.hasher.DynamicHasher;
+import org.apache.commons.collections4.bloomfilter.hasher.HashFunctionIdentity;
 import org.apache.commons.collections4.bloomfilter.hasher.Hasher;
 import org.apache.commons.collections4.bloomfilter.hasher.Shape;
 import org.apache.commons.collections4.bloomfilter.hasher.function.MD5Cyclic;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.PrimitiveIterator.OfInt;
 
 /**
  * Tests the {@link HasherBloomFilter}.
@@ -51,5 +56,42 @@ public class HasherBloomFilterTest extends AbstractBloomFilterTest {
     @Override
     protected HasherBloomFilter createFilter(final Hasher hasher, final Shape shape) {
         return new HasherBloomFilter(hasher, shape);
+    }
+
+    /**
+     * Test the edge case where the filter is empty and the getBits() function returns a
+     * zero length array.
+     */
+    @Test
+    public void getBitsTest_Empty() {
+        BloomFilter filter = createEmptyFilter(shape);
+        Assert.assertArrayEquals(new long[0], filter.getBits());
+    }
+
+    /**
+     * Test the edge case where the filter has only 1 bit in the lowest index and the getBits()
+     * function returns an array of length 1.
+     */
+    @Test
+    public void getBitsTest_LowestBitOnly() {
+        BloomFilter filter = createEmptyFilter(shape);
+        // Set the lowest bit index only.
+        filter.merge(new Hasher() {
+            @Override
+            public OfInt getBits(Shape shape) {
+                return Arrays.stream(new int[] {0}).iterator();
+            }
+
+            @Override
+            public HashFunctionIdentity getHashFunctionIdentity() {
+                return shape.getHashFunctionIdentity();
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        });
+        Assert.assertArrayEquals(new long[] {1L}, filter.getBits());
     }
 }
