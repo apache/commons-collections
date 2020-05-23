b043d821d6c1b23784d9e7935146d5c40eab69f4
dota17 <chenguopingdota@163.com>
2020-05-23 14:12:44 +0800

    Remove the unused import.

--- src/test/java/org/apache/commons/collections4/bloomfilter/AbstractBloomFilterTest.java
+++ src/test/java/org/apache/commons/collections4/bloomfilter/AbstractBloomFilterTest.java
@@ -19,7 +19,6 @@ package org.apache.commons.collections4.bloomfilter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
