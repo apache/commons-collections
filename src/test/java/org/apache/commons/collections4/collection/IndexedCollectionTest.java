8ce3f9a199205105556c1cca5c0e8f8e84d3ec3f
Gary Gregory <garydgregory@gmail.com>
2020-11-20 21:23:38 -0500

    Remove unused import.

--- src/test/java/org/apache/commons/collections4/collection/IndexedCollectionTest.java
+++ src/test/java/org/apache/commons/collections4/collection/IndexedCollectionTest.java
@@ -22,7 +22,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.commons.collections4.Transformer;
 
