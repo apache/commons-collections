0f090be501fc46a2a404e09577b63ca5f4bbcd96
HiFromAjay <kajay.jha@gmail.com>
2021-02-08 17:04:50 -0700

    add test cases for contains method

--- src/test/java/org/apache/commons/collections4/ArrayUtilsTest.java
+++ src/test/java/org/apache/commons/collections4/ArrayUtilsTest.java
@@ -17,6 +17,8 @@
 package org.apache.commons.collections4;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +36,32 @@ public class ArrayUtilsTest {
         assertEquals(3, ArrayUtils.indexOf(array, "3"));
         assertEquals(4, ArrayUtils.indexOf(array, null));
         assertEquals(-1, ArrayUtils.indexOf(array, "notInArray"));
+    }
+
+    @Test
+    public void testContains() {
+        final Object[] array = new Object[] { "0", "1", "2", "3", null, "0" };
+        assertFalse(ArrayUtils.contains(null, null));
+        assertFalse(ArrayUtils.contains(null, "1"));
+        assertTrue(ArrayUtils.contains(array, "0"));
+        assertTrue(ArrayUtils.contains(array, "1"));
+        assertTrue(ArrayUtils.contains(array, "2"));
+        assertTrue(ArrayUtils.contains(array, "3"));
+        assertTrue(ArrayUtils.contains(array, null));
+        assertFalse(ArrayUtils.contains(array, "notInArray"));
+    }
+
+    @Test
+    public void testContains_LANG_1261() {
+        class LANG1261ParentObject {
+            @Override
+            public boolean equals(final Object o) {
+                return true;
+            }
+        }
+        class LANG1261ChildObject extends LANG1261ParentObject {
+        }
+        final Object[] array = new LANG1261ChildObject[] { new LANG1261ChildObject() };
+        assertTrue(ArrayUtils.contains(array, new LANG1261ParentObject()));
     }
 }
