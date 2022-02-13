4c2a9744ae52b76a9aeaca15c90e68b672aa81e9
Gary Gregory <garydgregory@gmail.com>
2022-02-13 12:24:42 -0500

    Remove unused import.

--- src/test/java/org/apache/commons/collections4/junit/AbstractAvailableLocalesTest.java
+++ src/test/java/org/apache/commons/collections4/junit/AbstractAvailableLocalesTest.java
@@ -21,7 +21,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
