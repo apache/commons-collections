405ad435e5bba05e43bc72e5b2a270d1f56d8ec3
Gary Gregory <garydgregory@gmail.com>
2024-06-23 08:42:45 -0400

    Add generic Javadoc parameter

--- src/main/java/org/apache/commons/collections4/functors/ExceptionPredicate.java
+++ src/main/java/org/apache/commons/collections4/functors/ExceptionPredicate.java
@@ -24,6 +24,7 @@ import org.apache.commons.collections4.Predicate;
 /**
  * Predicate implementation that always throws an exception.
  *
+ * @param <T> the type of the input to the predicate.
  * @since 3.0
  */
 public final class ExceptionPredicate<T> implements Predicate<T>, Serializable {
