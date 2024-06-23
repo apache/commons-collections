b32379cefea19ba107d0cfa6f4660cc50b398678
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:09:01 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/set/UnmodifiableSortedSet.java
+++ src/main/java/org/apache/commons/collections4/set/UnmodifiableSortedSet.java
@@ -99,7 +99,7 @@ public final class UnmodifiableSortedSet<E>
     }
 
     /**
-     * Read the collection in using a custom routine.
+     * Deserializes the collection in using a custom routine.
      *
      * @param in  the input stream
      * @throws IOException if an error occurs while reading from the stream
