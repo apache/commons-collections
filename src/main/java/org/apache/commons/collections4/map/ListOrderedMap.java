0add2eabc9ea5c9fd6fda66fbf4174f132d348ab
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:01:57 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/ListOrderedMap.java
+++ src/main/java/org/apache/commons/collections4/map/ListOrderedMap.java
@@ -669,7 +669,7 @@ public class ListOrderedMap<K, V>
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in  the input stream
      * @throws IOException if an error occurs while reading from the stream
