c1287cad61ff4855139937889aa85f4c44817163
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:06:25 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/multimap/HashSetValuedHashMap.java
+++ src/main/java/org/apache/commons/collections4/multimap/HashSetValuedHashMap.java
@@ -117,15 +117,22 @@ public class HashSetValuedHashMap<K, V> extends AbstractSetValuedMap<K, V>
         return new HashSet<>(initialSetCapacity);
     }
 
-    private void readObject(final ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        ois.defaultReadObject();
+    /**
+     * Deserializes an instance from an ObjectInputStream.
+     *
+     * @param in The source ObjectInputStream.
+     * @throws IOException            Any of the usual Input/Output related exceptions.
+     * @throws ClassNotFoundException A class of a serialized object cannot be found.
+     */
+    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
         setMap(new HashMap<>());
-        doReadObject(ois);
+        doReadObject(in);
     }
 
-    private void writeObject(final ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        doWriteObject(oos);
+    private void writeObject(final ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        doWriteObject(out);
     }
 
 }
