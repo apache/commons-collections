1a10083dfaaa50bd9d0470ae5049440f26b8bd7f
Gary Gregory <garydgregory@gmail.com>
2024-06-23 08:42:48 -0400

    Add generic Javadoc parameter

--- src/main/java/org/apache/commons/collections4/functors/FalsePredicate.java
+++ src/main/java/org/apache/commons/collections4/functors/FalsePredicate.java
@@ -23,6 +23,7 @@ import org.apache.commons.collections4.Predicate;
 /**
  * Predicate implementation that always returns false.
  *
+ * @param <T> the type of the input to the predicate.
  * @since 3.0
  */
 public final class FalsePredicate<T> implements Predicate<T>, Serializable {
