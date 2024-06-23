d78084265eca1b3a1a6a04c2ca9268a748be6191
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:25:34 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/bag/CollectionSortedBag.java
+++ src/main/java/org/apache/commons/collections4/bag/CollectionSortedBag.java
@@ -135,13 +135,11 @@ public final class CollectionSortedBag<E> extends AbstractSortedBagDecorator<E> 
         return decorated().retainAll(null);
     }
 
-    // Bag interface
-
     /**
-     * Write the collection out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
