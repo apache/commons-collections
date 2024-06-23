237fc0bff6658279252316f976a7f8a4c5d74385
Gary Gregory <garydgregory@gmail.com>
2024-06-23 15:59:19 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/FixedSizeMap.java
+++ src/main/java/org/apache/commons/collections4/map/FixedSizeMap.java
@@ -135,7 +135,7 @@ public class FixedSizeMap<K, V>
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in  the input stream
      * @throws IOException if an error occurs while reading from the stream
