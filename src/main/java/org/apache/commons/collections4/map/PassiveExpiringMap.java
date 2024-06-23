e82dbe3512800b50955662e56089787ef276fd2f
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:32:05 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/PassiveExpiringMap.java
+++ src/main/java/org/apache/commons/collections4/map/PassiveExpiringMap.java
@@ -522,10 +522,10 @@ public class PassiveExpiringMap<K, V>
     }
 
     /**
-     * Write the map out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      */
     private void writeObject(final ObjectOutputStream out)
         throws IOException {
