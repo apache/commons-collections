506edf60c37e4009c368964eebc5f14da472e8df
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:36:20 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/set/UnmodifiableNavigableSet.java
+++ src/main/java/org/apache/commons/collections4/set/UnmodifiableNavigableSet.java
@@ -191,10 +191,10 @@ public final class UnmodifiableNavigableSet<E>
     }
 
     /**
-     * Write the collection out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
