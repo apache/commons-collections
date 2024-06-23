9fa70d9459cf57caba592252718f1d21cd139c87
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:04:12 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/ReferenceMap.java
+++ src/main/java/org/apache/commons/collections4/map/ReferenceMap.java
@@ -174,7 +174,7 @@ public class ReferenceMap<K, V> extends AbstractReferenceMap<K, V> implements Se
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in the input stream
      * @throws IOException if an error occurs while reading from the stream
