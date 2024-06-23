6c11a3422ad4d75cbd3cbf89e3b7a6820066c864
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:04:56 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/UnmodifiableMap.java
+++ src/main/java/org/apache/commons/collections4/map/UnmodifiableMap.java
@@ -120,7 +120,7 @@ public final class UnmodifiableMap<K, V>
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in  the input stream
      * @throws IOException if an error occurs while reading from the stream
