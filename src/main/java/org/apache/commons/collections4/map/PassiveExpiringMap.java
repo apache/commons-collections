5c1206fc23ab8ed8686707a006e10c0026c3f3e0
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:03:04 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/PassiveExpiringMap.java
+++ src/main/java/org/apache/commons/collections4/map/PassiveExpiringMap.java
@@ -444,7 +444,7 @@ public class PassiveExpiringMap<K, V>
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in the input stream
      * @throws IOException if an error occurs while reading from the stream
