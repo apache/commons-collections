7002d3ac4b971c13a2167ba32f9be3348c8a5c85
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:32:22 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/PredicatedMap.java
+++ src/main/java/org/apache/commons/collections4/map/PredicatedMap.java
@@ -173,10 +173,10 @@ public class PredicatedMap<K, V>
     }
 
     /**
-     * Write the map out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      * @since 3.1
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
