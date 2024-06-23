2f30f6f32330e6ec9868701c3b7925dc7075318a
Gary Gregory <garydgregory@gmail.com>
2024-06-23 08:43:05 -0400

    Add generic Javadoc parameter

--- src/main/java/org/apache/commons/collections4/functors/UniquePredicate.java
+++ src/main/java/org/apache/commons/collections4/functors/UniquePredicate.java
@@ -26,6 +26,7 @@ import org.apache.commons.collections4.Predicate;
  * Predicate implementation that returns true the first time an object is
  * passed into the predicate.
  *
+ * @param <T> the type of the input to the predicate.
  * @since 3.0
  */
 public final class UniquePredicate<T> implements Predicate<T>, Serializable {
