8f76fc2055820dbe02ac1e0272b983a9a96fd452
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:31:48 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/MultiValueMap.java
+++ src/main/java/org/apache/commons/collections4/map/MultiValueMap.java
@@ -566,10 +566,10 @@ public class MultiValueMap<K, V> extends AbstractMapDecorator<K, Object> impleme
     }
 
     /**
-     * Write the map out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      * @since 4.0
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
