b59254b0ef5ddd0bd89dc0fdf799d332b50bdadd
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:32:39 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/ReferenceIdentityMap.java
+++ src/main/java/org/apache/commons/collections4/map/ReferenceIdentityMap.java
@@ -240,10 +240,10 @@ public class ReferenceIdentityMap<K, V> extends AbstractReferenceMap<K, V> imple
     }
 
     /**
-     * Write the map out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
