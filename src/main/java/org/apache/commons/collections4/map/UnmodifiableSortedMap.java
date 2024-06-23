50cdd26c003d676f65ebe11197f605ffab9c63db
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:05:40 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/UnmodifiableSortedMap.java
+++ src/main/java/org/apache/commons/collections4/map/UnmodifiableSortedMap.java
@@ -126,7 +126,7 @@ public final class UnmodifiableSortedMap<K, V>
     }
 
     /**
-     * Read the map in using a custom routine.
+     * Deserializes the map in using a custom routine.
      *
      * @param in  the input stream
      * @throws IOException if an error occurs while reading from the stream
