2eb2f2673c6dd0d39dab841024c962e878c43e8f
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:31:14 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/map/ListOrderedMap.java
+++ src/main/java/org/apache/commons/collections4/map/ListOrderedMap.java
@@ -777,10 +777,10 @@ public class ListOrderedMap<K, V>
     }
 
     /**
-     * Write the map out using a custom routine.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      * @since 3.1
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
