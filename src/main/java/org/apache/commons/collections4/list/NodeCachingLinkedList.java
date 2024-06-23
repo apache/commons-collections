f52de4a192a263540934843556a0777f9140b19f
Gary Gregory <garydgregory@gmail.com>
2024-06-23 16:28:41 -0400

    Javadoc

--- src/main/java/org/apache/commons/collections4/list/NodeCachingLinkedList.java
+++ src/main/java/org/apache/commons/collections4/list/NodeCachingLinkedList.java
@@ -236,10 +236,10 @@ public class NodeCachingLinkedList<E> extends AbstractLinkedList<E> implements S
     }
 
     /**
-     * Serializes the data held in this object to the stream specified.
+     * Serializes this object to an ObjectOutputStream.
      *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
+     * @param out the target ObjectOutputStream.
+     * @throws IOException thrown when an I/O errors occur writing to the target stream.
      */
     private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
