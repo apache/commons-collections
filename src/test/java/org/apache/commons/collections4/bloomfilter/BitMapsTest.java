6078669ec9453d62aa62df1c940dedf4e362ae56
Sigee <sigee15@gmail.com>
2024-07-23 10:56:57 +0200

    Use consistent junit (jupiter) api everywhere

--- src/test/java/org/apache/commons/collections4/bloomfilter/BitMapsTest.java
+++ src/test/java/org/apache/commons/collections4/bloomfilter/BitMapsTest.java
@@ -16,9 +16,9 @@
  */
 package org.apache.commons.collections4.bloomfilter;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
