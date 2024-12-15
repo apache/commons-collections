ffe1af2585057dadfd013ab714ff2ceefa8b394a
Gary D. Gregory <garydgregory@gmail.com>
2024-12-15 10:02:25 -0500

    Javadoc

--- src/main/java/org/apache/commons/collections4/Get.java
+++ src/main/java/org/apache/commons/collections4/Get.java
@@ -30,63 +30,77 @@ import java.util.Set;
 public interface Get<K, V> {
 
     /**
+     * Tests for presence of a given key.
+     *
      * @param key key whose presence in this map is to be tested
-     * @return {@code true} if this map contains a mapping for the specified
-     *         key
+     * @return {@code true} if this map contains a mapping for the specified key
      * @see java.util.Map#containsKey(Object)
      */
     boolean containsKey(Object key);
 
     /**
+     * Tests for presence of a given value.
+     *
      * @param value value whose presence in this map is to be tested
-     * @return {@code true} if this map maps one or more keys to the
-     *         specified value
+     * @return {@code true} if this map maps one or more keys to the specified value
      * @see java.util.Map#containsValue(Object)
      */
     boolean containsValue(Object value);
 
     /**
-     * @return a set view of the mappings contained in this map
+     * Gets a set view of the mappings contained in this map.
+     *
+     * @return a set view of the mappings contained in this map.
      * @see java.util.Map#entrySet()
      */
     Set<java.util.Map.Entry<K, V>> entrySet();
 
     /**
+     * Gets a value at a given key.
+     *
      * @param key the key whose associated value is to be returned
-     * @return the value to which the specified key is mapped, or
-     *         {@code null} if this map contains no mapping for the key
+     * @return the value to which the specified key is mapped, or {@code null} if this map contains no mapping for the key
      * @see java.util.Map#get(Object)
      */
     V get(Object key);
 
     /**
+     * Tests whether this instance contains any key-value mappings.
+     *
      * @return {@code true} if this map contains no key-value mappings
      * @see java.util.Map#isEmpty()
      */
     boolean isEmpty();
 
     /**
+     * Gets a view of the keys contained in this map.
+     *
      * @return a set view of the keys contained in this map
      * @see java.util.Map#keySet()
      */
     Set<K> keySet();
 
     /**
+     * Remove a key-value mappings.
+     *
      * @param key key whose mapping is to be removed from the map
-     * @return the previous value associated with {@code key}, or
-     *         {@code null} if there was no mapping for {@code key}.
+     * @return the previous value associated with {@code key}, or {@code null} if there was no mapping for {@code key}.
      * @see java.util.Map#remove(Object)
      */
     V remove(Object key);
 
     /**
-     * @return the number of key-value mappings in this map
+     * Gets the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map.
      * @see java.util.Map#size()
      */
     int size();
 
     /**
-     * @return a collection view of the values contained in this map
+     * Gets a a collection view of the values contained in this map.
+     *
+     * @return a collection view of the values contained in this map.
      * @see java.util.Map#values()
      */
     Collection<V> values();
