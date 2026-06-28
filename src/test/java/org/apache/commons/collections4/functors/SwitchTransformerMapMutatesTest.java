95283b08ea1ec83b09b4bb8ae0c8004da5476b8d
Vasiliy Mikhailov <vasiliy-mikhailov@users.noreply.github.com>
2026-06-28 16:19:47 +0400

    Do not mutate the input map in SwitchTransformer.switchTransformer (#696)
    * Do not mutate the input map in SwitchTransformer.switchTransformer
    
    switchTransformer(Map) extracted the default with map.remove(null), which
    removes the null key from the caller list map as a side effect. Use
    map.get(null) so the factory method leaves the input map unchanged.
    
    * Declutter new test.
    
    * Fix mess in test.
    
    ---------
    
    Co-authored-by: Gary Gregory <garydgregory@users.noreply.github.com>

---
+++ src/test/java/org/apache/commons/collections4/functors/SwitchTransformerMapMutatesTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.collections4.functors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests that SwitchTransformer.switchTransformer(Map) does not mutate the input map.
+ */
+class SwitchTransformerMapMutatesTest {
+
+    /**
+     * Tests that switchTransformer(Map) does NOT mutate the input map by removing the null key.
+     */
+    @Test
+    void testSwitchTransformerMapDoesNotMutateInput() {
+        final Transformer<String, String> defaultTransformer = ConstantTransformer.constantTransformer("default");
+        final Transformer<String, String> transformer = ConstantTransformer.constantTransformer("value");
+        final Predicate<String> predicate = NullPredicate.nullPredicate();
+        final Map<Predicate<String>, Transformer<String, String>> map = new LinkedHashMap<>();
+        map.put(null, defaultTransformer);
+        map.put(predicate, transformer);
+        final int sizeBefore = map.size();
+        assertEquals(2, sizeBefore);
+        // Call the factory method
+        SwitchTransformer.switchTransformer(map);
+        // The map should NOT have been mutated - null key must still be present
+        final int sizeAfter = map.size();
+        assertEquals(sizeBefore, sizeAfter, "switchTransformer must not mutate the input map; expected size");
+    }
+}
