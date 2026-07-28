94ec20624f2a1fe982e196b51271a95a7028403d
Naveed Khan <dxbnaveed.k@gmail.com>
2026-07-28 12:52:46 +0530

    reject remove on the Unmodifiable map from SplitMapUtils.readableMap
    WrappedGet blocks clear, put and putAll and wraps every view it returns, but remove(Object) delegated to the wrapped Get, so the Unmodifiable view deleted from the underlying map. The Map default methods that route through remove inherited the hole.

--- src/main/java/org/apache/commons/collections4/SplitMapUtils.java
+++ src/main/java/org/apache/commons/collections4/SplitMapUtils.java
@@ -131,9 +131,15 @@ public class SplitMapUtils {
             throw new UnsupportedOperationException();
         }
 
+        /**
+         * Always throws {@link UnsupportedOperationException}.
+         *
+         * @param key Ignored.
+         * @throws UnsupportedOperationException Always thrown.
+         */
         @Override
         public V remove(final Object key) {
-            return get.remove(key);
+            throw new UnsupportedOperationException();
         }
 
         @Override
