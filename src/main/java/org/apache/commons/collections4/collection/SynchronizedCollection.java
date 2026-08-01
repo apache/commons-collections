da3fe232230be0cb087101e3e3ec3f5240ffdcd5
Naveed Khan <dxbnaveed.k@gmail.com>
2026-08-01 14:46:16 +0530

    synchronize forEach in SynchronizedCollection

--- src/main/java/org/apache/commons/collections4/collection/SynchronizedCollection.java
+++ src/main/java/org/apache/commons/collections4/collection/SynchronizedCollection.java
@@ -20,6 +20,7 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -139,6 +140,16 @@ public class SynchronizedCollection<E> implements Collection<E>, Serializable {
                 return true;
             }
             return object == this || decorated().equals(object);
+        }
+    }
+
+    /**
+     * @since 4.6.0
+     */
+    @Override
+    public void forEach(final Consumer<? super E> action) {
+        synchronized (lock) {
+            decorated().forEach(action);
         }
     }
 
